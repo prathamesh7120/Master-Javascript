b665feb0b9ee11748c5970b7843530032d3304a0
Prathamesh Chavan <88970197+prathamesh7120@users.noreply.github.com>
2025-04-15 12:26:12 +0000

    String and Maths

---
+++ 01_basics/05_string.js
@@ -0,0 +1,38 @@
+
+const name = "prathamesh";
+
+const repoCount =  50;
+
+// modern way
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('prathameshc-11');
+
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(-8,4);
+
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4);
+
+console.log(anotherString);
+
+
+const newStringOne = "  hitesh  ";
+
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+
---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,47 @@
+
+const score = 100;
+
+console.log(score);
+
+
+const balance = new Number(100);
+
+console.log(balance);
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(1));
+
+
+const otherNumber = 123.8986;
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++ MATHS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(4));
+
+
+// console.log(Math.round(2.7));
+
+
+// console.log(Math.ceil(4.2));
+
+
+// console.log(Math.floor(4.9));
+
+
+// console.log(Math.min(4,5,6,7));
+
+
+// console.log(Math.max(2,3,45,78,89));
+
+
+
+console.log(Math.random()*10);
