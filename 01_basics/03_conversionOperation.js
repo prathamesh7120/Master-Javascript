b72a8f1045b926b126e5a245ca0adb3c614c5232
Prathamesh Chavan <88970197+prathamesh7120@users.noreply.github.com>
2025-04-15 06:14:06 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+
+// let score  = "prathamesh";
+
+// console.log(typeof score);
+
+// let ValueInNumber = Number(score);
+
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = "prathamesh";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+// true => 1; false => 0
+// "" => false 
+// "prathamesh" => true
+
+
+let someNumber = 33;
+
+let StringNumber = String(someNumber);
+
+console.log(typeof StringNumber);
+
+
